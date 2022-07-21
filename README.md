9e6bbba54b568824d295a7583c4210f7c96f4673
Oluegwu Chigozie <59759364+oluegwuc@users.noreply.github.com>
2022-07-21 21:42:58 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple React Budget App
+
+This is a simple react budget demo create with `create-react-app` cmd.
+Clone the repo and run `npm install` cmd to install dependencies.
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../images/logo.png";
+
+function Header({ moneySymbol, totalBalance }) {
+  return (
+    <>
+      <img className='logo' src={logo} alt='' />
+      <div className='title-container'>
+        <h2 className='title'>
+          <span>Oca</span>WebTech
+        </h2>
+        <h3 className='subtitle'>Budget App</h3>
+      </div>
+
+      <div className='balance-container'>
+        <h2 className='label'>Balance</h2>
+        <h2 className='value'>
+          {moneySymbol}
+          {Math.abs(totalBalance)}
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default Header;
